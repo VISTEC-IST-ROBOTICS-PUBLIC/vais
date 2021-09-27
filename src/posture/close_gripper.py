dfdd2eedce6548029aee9e08eb1b5627c818a528
tjb16147 <act13027@hotmail.com>
2021-09-27 15:55:06 +0700

    add posture section

---
+++ src/posture/close_gripper.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import rospy
+import math
+from std_msgs.msg import String
+from sensor_msgs.msg import JointState
+from movo_action_clients.gripper_action_client import GripperActionClient
+from std_msgs.msg import Bool
+import datetime as dt
+
+if __name__ == "__main__":
+
+    print("initialize gripper to close")
+    process_start_time = dt.datetime.now()
+    rospy.init_node("movo_close_gripper")
+
+    #movo_lfinger = GripperActionClient('left')
+    movo_rfinger = GripperActionClient('right')
+    rospy.sleep(2)
+
+#--------------------------------finger----------------------------------------------------
+    #movo_lfinger.command(0)#open
+    movo_rfinger.command(0)
+    #movo_lfinger.wait(3)
+    movo_rfinger.wait(3)
+
+
+
